385e4d87db46897d1c4c258925d21c3a8ca66674
unknown <luizsilvaa1114@gmail.com>
2020-10-24 18:48:00 -0300

    criar comidinha

--- script.js
+++ script.js
@@ -8,6 +8,10 @@ snake[0] = {
 }
 
 let direction = "right";
+let food = {
+    x: Math.floor(Math.random() * 15 + 1) * box,
+    y:Math.floor(Math.random()* 15 + 1) * box
+}
 
 function criarBG() {
     context.fillStyle = 'lightgreen';
@@ -20,6 +24,13 @@ function criarCobrinha() {
         context.fillRect(snake[i].x, snake[i].y, box, box);        
     }
 }
+
+function drawfood(){
+    context.fillStyle = "red"
+    context.fillRect( food.x, food.y, box, box);
+}
+
+
 
 document.addEventListener('keydown', update);
 function update(event){
@@ -37,6 +48,7 @@ function iniciarJogo() {
     
     criarBG();
     criarCobrinha();
+    drawfood();
  
     let snakeX = snake[0].x;
     let snakeY = snake[0].y;
